aff721acbf17f8011e44df348867b43bc1ba44f7
aditya-clecotech <aditya.parihar@clecotech.com>
2025-03-28 14:00:33 +0530

    Created authentication with omni_auth2

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,42 @@
+# app/controllers/users/omniauth_callbacks_controller.rb:
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+   # def google_oauth2
+   #    debugger
+   #     # You need to implement the method below in your model (e.g. app/models/user.rb)
+   #     @user = User.from_omniauth(request.env['omniauth.auth'])
+ 
+   #     if @user.persisted?
+   #       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+   #       sign_in_and_redirect @user, event: :authentication
+   #     else
+   #       # Useful for debugging login failures. Uncomment for development.
+   #       # session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+   #       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+   #     end
+   # end
+
+   def google_oauth2
+      user = User.from_google(from_google_params)
+ 
+      if user.present?
+        sign_out_all_scopes
+        flash[:notice] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect user, event: :authentication
+      else
+        flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
+        redirect_to new_user_session_path
+      end
+     end
+ 
+     def from_google_params
+       @from_google_params ||= {
+         uid: auth.uid,
+         email: auth.info.email
+       }
+     end
+ 
+     def auth
+       @auth ||= request.env['omniauth.auth']
+     end
+ end
